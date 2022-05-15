d903c35cec983675378af9845468c41455b57382
Jayce <99360903+hudware-console@users.noreply.github.com>
2022-05-16 00:41:14 +0300

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -5,7 +5,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "Jayce" && password === "D4gcXVvXGaih7YsW9LhQsWoEe") {
         alert("You have successfully logged in.");
-        window.location.href = "openViP.html";
+        window.location.href = "/GOOD/index.html";
         location.reload();
     } else {
         loginErrorMsg.style.opacity = 1;
