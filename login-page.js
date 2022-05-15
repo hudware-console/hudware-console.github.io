716d0121b810761fedcbbdcddadd0869db48c1b5
Jayce <99360903+hudware-console@users.noreply.github.com>
2022-05-16 00:30:45 +0300

    Create login-page.js

---
+++ login-page.js
@@ -0,0 +1,12 @@
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev") {
+        alert("You have successfully logged in.");
+        location.reload();
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
